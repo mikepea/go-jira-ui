ae16cce2ad711149423416848d585f120db4bbdb
Mike Pountney <mikepea@yelp.com>
2015-12-30 19:15:39 -0800

    Two page support - still sloppy, but working!
    This adds 'TicketList' page, giving the results of the selected query.
    
    No efforts have been made to do pagination, or give limits on up/down.
    
    Refreshing the screen between pages might be handy, though tbh the artifacts
    look quite nice.
    
    BUT IT'S WORKING!

--- main.go
+++ main.go
@@ -23,8 +23,14 @@ var exitNow = false
 var currentPage = ticketQuery
 var previousPage = ticketQuery
 
+var currentQuery = ""
+var previousQuery = ""
+
 var ticketSelected = 0
 var querySelected = 0
+
+var activeQueryList *ui.List
+var activeTicketList *ui.List
 
 func prevTicket(n int) {
 	ticketSelected = ticketSelected - n
@@ -48,17 +54,25 @@ type Query struct {
 }
 
 var origQueries = []Query{
-	Query{"My Tickets", "--project OPS AND owner = 'mikepea'"},
-	Query{"My Watched Tickets", "--project OPS AND watcher = 'mikepea'"},
-	Query{"unlabelled", "--project OPS AND labels IS EMPTY"},
-	Query{"Ops Queue", "--project OPS"},
+	Query{"My Tickets", "project = OPS AND assignee = 'mikepea' AND resolution = Unresolved"},
+	Query{"My Watched Tickets", "project = OPS AND watcher = 'mikepea' AND resolution = Unresolved"},
+	Query{"unlabelled", "project = OPS AND labels IS EMPTY AND resolution = Unresolved"},
+	Query{"Ops Queue", "project = OPS AND resolution = Unresolved"},
 }
 
-var queries = []string{
+var displayQueries = []string{
 	"My Tickets",
 	"My Watched Tickets",
 	"unlabelled",
 	"OPS queue",
+}
+
+var currentTicketListCache []string
+var displayTickets []string
+
+func displayQueryResults(query string) []string {
+	results := JiraQueryAsStrings(query)
+	return results
 }
 
 func markActiveQuery() {
@@ -67,62 +81,57 @@ func markActiveQuery() {
 		if i == querySelected {
 			selected = "fg-white,bg-blue"
 		}
-		queries[i] = fmt.Sprintf("[%s](%s)", v.Name, selected)
+		displayQueries[i] = fmt.Sprintf("[%s](%s)", v.Name, selected)
 	}
 }
 
-func updateQueries(ls *ui.List) {
-	markActiveQuery()
-	ls.Items = queries
-	ui.Render(ls)
-}
-
-/*
-func nextPage() {
-	if currentPage == ticketList {
-		currentPage = ticketShow
-	} else if currentPage == ticketShow {
-		currentPage = ticketQuery
-	} else if currentPage == ticketQuery {
-		currentPage = ticketList
+func markActiveTicket() {
+	for i, v := range currentTicketListCache {
+		selected := ""
+		if i == ticketSelected {
+			selected = "fg-white,bg-blue"
+		}
+		displayTickets[i] = fmt.Sprintf("[%s](%s)", v, selected)
 	}
-	ui.StopLoop()
 }
-*/
 
 func handleTicketQueryPage() {
-
-	err := ui.Init()
-	if err != nil {
-		panic(err)
-	}
-	defer ui.Close()
-
 	ls := ui.NewList()
-	ls.Items = queries
+	ls.Items = displayQueries
 	ls.ItemFgColor = ui.ColorYellow
 	ls.BorderLabel = "List"
 	ls.Height = 10
 	ls.Width = 80
 	ls.Y = 0
+	activeQueryList = ls
 	markActiveQuery()
 	ui.Render(ls)
+}
 
-	ui.Handle("/sys/kbd/q", func(ui.Event) {
-		ui.StopLoop()
-		exitNow = true
-	})
-	ui.Handle("/sys/kbd/j", func(ui.Event) {
-		nextQuery(1)
-		updateQueries(ls)
-	})
-	ui.Handle("/sys/kbd/k", func(ui.Event) {
-		prevQuery(1)
-		updateQueries(ls)
-	})
+func changePage() {
+	switch currentPage {
+	case ticketQuery:
+		handleTicketQueryPage()
+	case ticketList:
+		handleTicketListPage()
+	case ticketShow:
+	}
+}
 
-	ui.Loop()
-
+func handleTicketListPage() {
+	ticketSelected = 0
+	currentTicketListCache = displayQueryResults(origQueries[querySelected].JQL)
+	displayTickets = make([]string, len(currentTicketListCache))
+	ls := ui.NewList()
+	ls.Items = displayTickets
+	ls.ItemFgColor = ui.ColorYellow
+	ls.BorderLabel = "List"
+	ls.Height = 30
+	ls.Width = 132
+	ls.Y = 0
+	activeTicketList = ls
+	markActiveTicket()
+	ui.Render(ls)
 }
 
 func getJiraOpts() map[string]interface{} {
@@ -141,7 +150,7 @@ func getJiraOpts() map[string]interface{} {
 		"sort":        defaultSort,
 		"max_results": defaultMaxResults,
 		"method":      "GET",
-		"quiet":       false,
+		"quiet":       true,
 	}
 	//opts := make(map[string]interface{})
 	return defaults
@@ -164,6 +173,67 @@ func JiraQueryAsStrings(query string) []string {
 	return strings.Split(buf.String(), "\n")
 }
 
+func updateQueryPage(ls *ui.List) {
+	markActiveQuery()
+	ls.Items = displayQueries
+	ui.Render(ls)
+}
+
+func updateTicketListPage(ls *ui.List) {
+	markActiveTicket()
+	ui.Render(ls)
+}
+
+func registerKeyboardHandlers() {
+	ui.Handle("/sys/kbd/q", func(ui.Event) {
+		ui.StopLoop()
+		exitNow = true
+	})
+	ui.Handle("/sys/kbd/j", func(ui.Event) {
+		handleDownKey()
+	})
+	ui.Handle("/sys/kbd/k", func(ui.Event) {
+		handleUpKey()
+	})
+	ui.Handle("/sys/kbd/l", func(ui.Event) {
+		handleSelectKey()
+	})
+}
+
+func handleSelectKey() {
+	switch currentPage {
+	case ticketQuery:
+		currentPage = ticketList
+		previousPage = ticketQuery
+	case ticketList:
+		currentPage = ticketQuery
+		previousPage = ticketList
+	}
+	changePage()
+}
+
+func handleUpKey() {
+	switch currentPage {
+	case ticketQuery:
+		prevQuery(1)
+		updateQueryPage(activeQueryList)
+	case ticketList:
+		prevTicket(1)
+		updateTicketListPage(activeTicketList)
+	}
+}
+
+func handleDownKey() {
+	switch currentPage {
+	case ticketQuery:
+		nextQuery(1)
+		updateQueryPage(activeQueryList)
+	case ticketList:
+		nextTicket(1)
+		updateTicketListPage(activeTicketList)
+	}
+}
+
 var (
 	log    = logging.MustGetLogger("jira")
 	format = "%{color}%{time:2006-01-02T15:04:05.000Z07:00} %{level:-5s} [%{shortfile}]%{color:reset} %{message}"
@@ -177,23 +247,27 @@ func main() {
 
 	c := jira.New(opts)
 
-	// check to see if we can run a query, otherwise force a login
 	// TODO: make this quicker somehow
 	if _, err := runJiraQuery("assignee = 'mikepea' AND resolution = Unresolved"); err != nil {
-		//fmt.Println(err)
 		c.CmdLogin()
 	}
 
-	lines := JiraQueryAsStrings("assignee = 'mikepea' AND resolution = Unresolved")
-	fmt.Println(lines)
-
 	// debug pause
-	//time.Sleep(2 * time.Millisecond)
-	time.Sleep(5 * time.Second)
+	time.Sleep(2 * time.Millisecond)
+	//time.Sleep(5 * time.Second)
+
+	err := ui.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer ui.Close()
+
+	registerKeyboardHandlers()
 
 	for exitNow != true {
 
 		handleTicketQueryPage()
+		ui.Loop()
 
 	}
 
