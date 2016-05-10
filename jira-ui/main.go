2c4fcfacccfe74ddf36ffb79829242a1de364e27
Mike Pountney <mikepea@yelp.com>
2016-05-10 06:16:49 -0700

    Do a reset of the terminal on exit
    ... since we tend to screw around with it and sometimes that goes wrong

--- jira-ui/main.go
+++ jira-ui/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"os/exec"
+
 	"github.com/mikepea/go-jira-ui"
 )
 
+func resetTTY() {
+	cmd := exec.Command("reset")
+	_ = cmd.Run()
+}
+
 func main() {
+	defer resetTTY()
 	jiraui.Run()
 }
